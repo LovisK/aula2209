0a4aaaf969345e5e03315a991b4be3be7ddc85be
LovisK <111066809+LovisK@users.noreply.github.com>
2022-09-22 12:06:39 -0300

    aula de ho

---
+++ segundoprojeto/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sengundorojeto';
+  nome = 'murillo';
+  idade = 18;
+
+  esconderCaixa = true;
+
+  numero: number | undefined;
+  
+escolher (){
+this.numero = Math.floor(Math.random() * 100) + 1;
+}
+
+  alterarNome(nome: any) {
+    console.log(nome.target.value);
+    this.nome = nome.target.value;
+  
+    }
+  lancarDado() {
+    return Math.floor(Math.random() * 6) + 1;
+    }
+    adicionar(nomeInput: any) {
+      console.log(nomeInput.value);
+      this.nome = nomeInput.value;
+      this.esconderCaixa = nomeInput.value.length <= 0;
+      }
+}
